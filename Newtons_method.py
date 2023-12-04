8cfc3cb692526dbdd923081680619cef31385755
Neil <77575319+Armweak5104@users.noreply.github.com>
2023-12-04 15:42:48 +0800

    Update Newtons_method.py

--- Newtons_method.py
+++ Newtons_method.py
@@ -1,7 +1,21 @@
 from scipy.misc import derivative
+import random
+degree = input("What degree polynomial would you like to use"]
+coeffs = []
+for i in range(int(degree)):
+  k = int(input(f"Enter coefficient for exponent {i}"))
+  coeffs.append(k)
 def f(x):
-  return x**2 +5*x + 1
-sol = int(input("What is your first guess:"))
+  out = 0
+  for i in range(len(coeffs)):
+    out+=coeffs[i]*(x**i)
+  return out
+
+initial_range = input("Enter a random range for which your polynomial is continuous eg [-100,100]")
+initial_range = initial_range[1:-1]
+initial_range.split(",")
+sol = random.randint(int(initial_range[0]),int(initial_range[2]))
+
 n = int(input("How many iterations would you like to run:"))
 approximate = None
 for i in range(1,n):
